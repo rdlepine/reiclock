08cfc6ee7d554f657092f2c509ffd5795b8c6f0b
Lepine <rlepine@rocelec.com>
2019-04-19 22:28:12 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,192 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Flag from 'react-world-flags'
+import Moment from 'react-moment';
+
 import './App.css';
 
+const styles = {
+  card: {
+    margin: 20,
+    maxWidth: 345,
+    width: 200,
+    height: 200,
+  },
+  media: {
+    height: 140,
+  },
+  content: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifContent: 'space-between',
+  },
+  title: {
+    paddingTop: 30,
+    width: '100%',
+    align: 'center',
+  },
+  flag: {
+    height: 100,
+    width: 100,
+  }
+};
+
 class App extends Component {
+
+
   render() {
+
+    const { classes } = this.props;
+    const today = new Date();
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+      <Typography variant="display1" align="center" component="p" className={classes.title}>
+        ROCHESTER ELECTRONICS OFFICE CLOCKS
+      </Typography>
+      <div  className={classes.content}>
+        <Card className={classes.card}>
+            <CardActionArea>
+              <CardContent>
+                <Flag code="us" height="64" />
+                <Typography gutterBottom variant="h5" component="h2">
+                  BOSTON
+                </Typography>
+                <Typography  component="p">
+                  <Moment interval={0} >
+                      {today}
+                  </Moment>
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+        </Card>
+
+        <Card className={classes.card}>
+            <CardActionArea>
+              <CardContent>
+                <Flag code="cn"  height="64"/>
+                <Typography gutterBottom variant="h5" component="h2">
+                  SHANGHAI
+                </Typography>
+                <Typography component="p">
+                  <Moment interval={0} >
+                    {today}
+                   </Moment>
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+        </Card>
+
+        <Card className={classes.card}>
+            <CardActionArea>
+              <CardContent>
+                <Flag code="jp"  height="64"/>
+                <Typography gutterBottom variant="h5" component="h2" >
+                  TOKYO
+                </Typography>
+                <Typography component="p">
+                  <Moment interval={0} >
+                     {today}
+                  </Moment>
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+        </Card>
+
+        <Card className={classes.card}>
+            <CardActionArea>
+              <CardContent>
+                <Flag code="de"  height="64"/>
+                <Typography gutterBottom variant="h5" component="h2">
+                  MUNICH
+                </Typography>
+                 <Typography component="p">
+                  <Moment interval={0} >
+                    {today}
+                  </Moment>
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+        </Card>
+
+      <Card className={classes.card}>
+        <CardActionArea>
+          <CardContent>
+            <Flag code="gb"  height="64"/>
+            <Typography gutterBottom variant="h5" component="h2">
+              LONDON
+            </Typography>
+            <Typography component="p">
+              <Moment interval={0} >
+                {today}
+              </Moment>
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+
+      <Card className={classes.card}>
+        <CardActionArea>
+          <CardContent>
+            <Flag code="us"  height="64"/>
+            <Typography gutterBottom variant="h5" component="h2">
+              PHOENIX
+            </Typography>
+            <Typography variant="subtitle1" component="p">
+              <Moment interval={0} >
+                {today}
+              </Moment>
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+
+      <Card className={classes.card}>
+      <CardActionArea>
+        <CardContent>
+          <Flag code="sg"  height="64"/>
+          <Typography gutterBottom variant="h5" component="h2">
+            SINGAPORE
+          </Typography>
+          <Typography component="p">
+            <Moment interval={0} >
+              {today}
+            </Moment>
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+
+
+      <Card className={classes.card}>
+        <CardActionArea>
+          <CardContent>
+            <Flag code="us"  height="64" />
+            <Typography gutterBottom variant="h5" component="h2">
+              MINNEAPOLIS
+            </Typography>
+            <Typography component="p">
+              <Moment interval={0} >
+                {today}
+              </Moment>
+            </Typography>
+          </CardContent>
+      </CardActionArea>
+    </Card>
+
       </div>
+    </div>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
